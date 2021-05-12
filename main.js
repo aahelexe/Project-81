0f9f559019e4cd4d7f63e2ed286a4810021f77be
aahelexe <78219629+aahelexe@users.noreply.github.com>
2021-05-12 21:51:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 var canvas= document.getElementById("myCanvas");
 var ctx= canvas.getContext("2d");
-var color = "red";
 ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=3;
-ctx.arc(200,200,40,0,2*Math.PI);
+ctx.strokeStyle="black";
+ctx.lineWidth=2;
+ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 canvas.addEventListener("mousedown", my_mousedown);
@@ -12,14 +11,33 @@ function my_mousedown(e)
 {
     mouse_y = e.clientY - canvas.offsetTop;
     mouse_x = e.clientX - canvas.offsetLeft;
-    console.log("Y= "+mouse_y, "X= "+mouse_x);
-    circle(mouse_x, mouse_y);
+    circle();
 }
-function circle(mouse_x, mouse_y) 
+function circle() 
 {
     ctx.beginPath();
-    ctx.strokeStyle=color;
+    ctx.strokeStyle="blue";
     ctx.lineWidth=3;
-    ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
+    ctx.arc(250, 210, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle="black";
+    ctx.lineWidth=3;
+    ctx.arc(350, 210, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle="red";
+    ctx.lineWidth=3;
+    ctx.arc(450, 210, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle="#fcba03";
+    ctx.lineWidth=3;
+    ctx.arc(290, 260, 40, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.strokeStyle="green";
+    ctx.lineWidth=3;
+    ctx.arc(400, 260, 40, 0, 2*Math.PI);
     ctx.stroke();
 }
